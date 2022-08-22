b7da98d0e6bf0090ac920464fe5ea8a484728681
named.josie <named.josie@gmail.com>
2022-08-21 20:42:36 -0400

    add question mark

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -4,15 +4,10 @@ import './styles/navbar.css'
 export default function NavBar() {
   return (
     <nav>
-      {/* <h3>
-        <Link to='/'>Home </Link>
-      </h3> */}
+     
       <h1 className='nav-title'>
         <Link to='/snacks'>Snacks <img className='cookie' src='https://cdn.shopify.com/s/files/1/0071/3195/5259/files/Cookie_Gif-300-80.gif?4519' alt='cookie'></img></Link>
       </h1>
-      {/* <h3>
-        <Link to='/about'>About Us</Link>
-      </h3> */}
       <button className='new-button'>
         <Link to='/snacks/new'>New</Link>
       </button>
